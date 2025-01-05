06eae310ec8bd850d759f33ec85ed119b062608e
mariana <eumarianamota@gmail.com>
2025-01-05 17:19:54 -0300

    'commit'

--- Bloco_I/orientacao_objetos/II_exercises/q07_event/Agenda.py
+++ Bloco_I/orientacao_objetos/II_exercises/q07_event/Agenda.py
@@ -9,8 +9,17 @@ class Agenda:
     
     def add_event(self, name, date, time, location):
         for i in self.events:
-            if i[0] == name and i[1] == time: 
+            if (i[0] == name) and (i[2] == time): 
                 raise EventConflicError('Já existe um evento com esse nome e data')
             else:
                 self.events.append([name, date, time, location])
-        print(self.events)
+    
+    def remove_event(self, name):
+        for i in self.events:
+            if i[0] == name:
+                self.events.remove(i)
+    
+    def list_events(self, date):
+        for i in self.events:
+            if i[1] == date:
+                print(f'{i[0]}, {i[1]}, {i[2]}, {i[3]}')
--- Bloco_I/orientacao_objetos/II_exercises/q07_event/test.py
+++ Bloco_I/orientacao_objetos/II_exercises/q07_event/test.py
@@ -4,7 +4,9 @@ from Agenda import Agenda
 casamento = Event('Nath e Lusca', '25/06/2022', '16:00', 'Paris, França')
 aniversario = Event('Mariana', '02/04/2025', '20:00', 'Minha casa')
 formatura = Event('Mariana Forms', '15/01/2027', '22:00', 'Faustino')
+confra = Event('Mariana', '15/01/2027', '22:00', 'Faustino')
+niver = Event('Lucy', '15/01/2027', '22:00', 'Faustino')
 
 agenda = Agenda()
-agenda.add_event('Mariana Forms', '15/01/2027', '22:00', 'Faustino')
+agenda.list_events('15/01/2027')
 
